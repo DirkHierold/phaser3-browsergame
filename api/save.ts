e39e5eab35cc525cefda82106bdd07a6923c1e10
Hierold <FV85JYT@vw.vwgroup.com>
2023-02-06 21:39:11 +0100

    read data 1

--- api/save.ts
+++ api/save.ts
@@ -1,9 +1,10 @@
-// import { readFileSync } from "fs";
+import { readFileSync } from "fs";
 
 export default function handler(req, res) {
   console.log("Save Api");
-  // const stringified = readFileSync("/data/highscores.json", "utf8");
+  const stringified = readFileSync("/data/highscores.json", "utf8");
 
   res.setHeader("Content-Type", "application/json");
-  return res.end("666");
+  console.log(stringified);
+  return res.end(stringified);
 }
